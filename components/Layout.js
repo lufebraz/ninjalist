b386cb24ab41ee6aa5691e469ffef2dde3d9508e
lufebraz <luizbraz98@gmail.com>
2021-05-27 19:44:59 -0300

    ajustando layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-
+import Head from 'next/head'
 const Layout = ({ children }) => {
   return (
     <div className="content">
+       <Head>
+      <title>Ninja List | Home</title>
+      </Head>
       <Navbar />
       {children}
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <Head>
-      <title>Ninja List | Home</title>
-      </Head>
+     
       <div>
         <h1 className={styles.title}>Homepage</h1>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et perspiciatis blanditiis, nulla vel officia veniam dolore iste quo tempore alias fugit reprehenderit minus animi ab, nihil in quaerat voluptatum eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, sit eveniet? Explicabo qui minima id? Maiores voluptas, quod optio distinctio eveniet autem. Quam nam consequuntur pariatur et magni minima voluptates?</p>
